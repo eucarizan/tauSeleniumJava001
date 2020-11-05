977ff0533d3fdeebe45a48a1c3f26487c350e7fa
eucarizan <eucarizan@gmail.com>
2020-11-05 21:13:41 +0800

    Added KeyPressesPage

---
+++ tau001/src/main/java/pages/KeyPressesPage.java
@@ -0,0 +1,33 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class KeyPressesPage {
+
+    private WebDriver driver;
+    private By inputField = By.id("target");
+    private By resultText = By.id("result");
+
+    public KeyPressesPage(WebDriver driver){
+        this.driver = driver;
+    }
+
+    public void enterText(String text){
+        driver.findElement(inputField).sendKeys(text);
+    }
+
+    public void enterPi(){
+        //Actions action = new Actions(driver);
+        //action.keyDown(Keys.ALT, Keys.NUMPAD2)
+        enterText("\u03C0" + "=3.14");
+        //enterText(Keys.ALT, "227");
+        // enterText(Keys.chord(Keys.ALT, "2") + "=3.14");
+    }
+
+    public String getResult(){
+        return driver.findElement(resultText).getText();
+    }
+}
---
+++ tau001/src/test/java/keys/KeysTests.java
@@ -0,0 +1,24 @@
+package keys;
+
+import org.openqa.selenium.Keys;
+import org.testng.annotations.Test;
+
+import base.BaseTests;
+
+import static org.testng.Assert.*;
+
+public class KeysTests extends BaseTests{
+    
+    @Test
+    public void testBackspace(){
+        var keyPage = homePage.clickKeyPresses();
+        keyPage.enterText("A" + Keys.BACK_SPACE);
+        assertEquals(keyPage.getResult(), "You entered: BACK_SPACE");
+    }
+
+    @Test
+    public void testPi(){
+        var keyPage = homePage.clickKeyPresses();
+        keyPage.enterPi();
+    }
+}
