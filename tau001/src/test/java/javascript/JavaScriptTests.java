64968dda015aac2dd9b5548ae010ff19e9c5041f
eucarizan <eucarizan@gmail.com>
2020-11-06 20:03:41 +0800

    Updated DropDownPage for a test in JavaScriptTests

--- tau001/src/test/java/javascript/JavaScriptTests.java
+++ tau001/src/test/java/javascript/JavaScriptTests.java
@@ -1,5 +1,7 @@
 package javascript;
 
+import java.util.List;
+import static org.testng.Assert.*;
 import org.testng.annotations.Test;
 import base.BaseTests;
 
@@ -13,5 +15,18 @@ public class JavaScriptTests extends BaseTests{
     @Test
     public void testScrollToFiftParagraph(){
         homePage.clickInfiniteScroll().scrollToParagraph(5);
+    }
+
+    @Test
+    public void testAddMultipleAttributeToDropdown(){
+        var dropDownPage = homePage.clickDropDown();
+        dropDownPage.addMultipleAttribute();
+
+        var optionsToSelect = List.of("Option 1", "Option 2");
+        optionsToSelect.forEach(dropDownPage::selectFromDropDown);
+
+        var selectedOptions = dropDownPage.getSelectedOptions();
+        assertTrue(selectedOptions.containsAll(optionsToSelect), "All options were not selected");
+        assertEquals(selectedOptions.size(), optionsToSelect.size(), "Number of selected items");
     }
 }
