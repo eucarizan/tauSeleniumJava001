1b936f246b9e1f1452d0d243892be6d7a634cc9a
eucarizan <eucarizan@gmail.com>
2020-11-06 19:22:34 +0800

    Added DynamicLoadingExample2Page and added test waitUntilVisible

--- tau001/src/main/java/pages/DynamicLoadingPage.java
+++ tau001/src/main/java/pages/DynamicLoadingPage.java
@@ -1,6 +1,7 @@
 package pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 
 public class DynamicLoadingPage {
@@ -8,14 +9,25 @@ public class DynamicLoadingPage {
     private WebDriver driver;
     private String linkXpath_Format = ".//a[contains(text(), '%s')]";
     private By link_Example1 = By.xpath(String.format(linkXpath_Format, "Example 1"));
+    private By link_Example2 = By.xpath(String.format(linkXpath_Format, "Example 2"));
 
     public DynamicLoadingPage(WebDriver driver){
         this.driver = driver;
     }
 
-    public DynamicLoadingPageExample1Page clickExample1(){
+    public DynamicLoadingExample1Page clickExample1(){
         driver.findElement(link_Example1).click();
-        return new DynamicLoadingPageExample1Page(driver);
+        return new DynamicLoadingExample1Page(driver);
+    }
+
+    public DynamicLoadingExample2Page clickExample2(){
+        driver.findElement(link_Example2).click();
+        return new DynamicLoadingExample2Page(driver);
+    }
+
+    public DynamicLoadingExample2Page rightClickOnExample2Link(){
+        driver.findElement(link_Example2).sendKeys(Keys.CONTROL, Keys.RETURN);
+        return new DynamicLoadingExample2Page(driver);
     }
 
 }
