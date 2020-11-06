66d79bb4765151d86772ec30e6ee96ea3af12022
eucarizan <eucarizan@gmail.com>
2020-11-07 00:57:52 +0800

    last. added CookieManager and CookiesTests

--- tau001/src/test/java/base/BaseTests.java
+++ tau001/src/test/java/base/BaseTests.java
@@ -5,10 +5,12 @@ import java.io.IOException;
 
 import com.google.common.io.Files;
 
+// import org.openqa.selenium.Cookie;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 // import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
@@ -19,6 +21,7 @@ import org.testng.annotations.BeforeMethod;
 import pages.HomePage;
 import utlis.EventReporter;
 import utlis.WindowManager;
+import utlis.CookieManager;
 
 public class BaseTests {
 
@@ -36,21 +39,24 @@ public class BaseTests {
     public void setUp() {
         System.setProperty("webdriver.chrome.driver", "tau001/rss/chromedriver.exe");
         // driver = new ChromeDriver();
-        driver = new EventFiringWebDriver(new ChromeDriver());
+        driver = new EventFiringWebDriver(new ChromeDriver(getChromeOptions()));
         driver.register(new EventReporter());
+        goHome();
+        // setCookie();
         // driver.get(lurl);
-        driver.get(url);
+        // driver.get(url);
         /*
          * try { Thread.sleep(2000); } catch (InterruptedException e) {
          * e.printStackTrace(); }
          */
 
-        homePage = new HomePage(driver);
+        // homePage = new HomePage(driver);
     }
 
     @BeforeMethod
     public void goHome() {
         driver.get(url);
+        homePage = new HomePage(driver);
     }
 
     @AfterClass
@@ -75,4 +81,24 @@ public class BaseTests {
     public WindowManager getWindowManager(){
         return new WindowManager(driver);
     }
+
+    private ChromeOptions getChromeOptions(){
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("disable-infobars");
+        // options.setHeadless(true);
+        return options;
+    }
+
+    public CookieManager getCookieManager(){
+        return new CookieManager(driver);
+    }
+   /* 
+    private void setCookie(){
+        Cookie cookie = new Cookie.Builder("tau", "123")
+            .domain("the-internet.herokuapp.com")
+            .build();
+
+            driver.manage().addCookie(cookie);
+    }
+    */
 }
